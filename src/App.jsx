b41b8d98490f9313eda62f5d59d0ca6ed575d1e0
Dhruvsharma <sharmadhruv1303@gmail.com>
2024-09-07 18:40:38 +0530

    Added 3d Portfole=io

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { Footer, Navbar } from "./Components";
+// import { About, Contact, Home, Projects } from "./pages";
+import About from "../src/Pages/About";
+import Contact from "../src/Pages/Contact";
+import Home from "../src/Pages/Home";
+import Projects from "../src/Pages/Projects";
+
+
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,16 @@
+
+import Alert from "./Alert";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Footer from "./Footer.jsx";
+import HomeInfo from "./HomeInfo";
+import CTA from "./CTA.jsx";
+
+export {
+    CTA,
+    Alert,
+    Loader,
+    Navbar,
+    Footer,
+    HomeInfo
+}
